b3e6c63585a92675a1a3b0b22c6dffb885b5f35b
Prannaya Gupta <prannayagupta@gmail.com>
2025-06-12 08:56:40 +0800

    feat: update with our implementation of the participant server

---
+++ finals/test.sh
@@ -0,0 +1,14 @@
+TEAM_NAME="2b3y"
+TEAM_TRACK="advanced"
+if [[ $TEAM_NAME =~ ^[0-9] ]]; then
+    # Starts with a number
+    repo_name=repo-til-25-${TEAM_NAME}
+else
+    # Starts with a letter
+    repo_name=${TEAM_NAME}-repo-til-25
+fi
+ar_ref=asia-southeast1-docker.pkg.dev/til-ai-2025/${repo_name}/${TEAM_NAME}-server:finals
+# Builds finals repo and pushes to Artifact Registry
+docker build -t $ar_ref finals && docker push $ar_ref
+# Run all containers from docker-compose.yml
+REPO_NAME=$repo_name docker compose up --build --force-recreate --abort-on-container-exit
